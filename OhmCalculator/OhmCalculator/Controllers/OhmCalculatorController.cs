e80aab2184c6d537326c461d99d8d3e77ec74882
nhemani <39281012+nhemani@users.noreply.github.com>
2018-05-14 16:01:46 -0500

    Initial Check in
    first check in

---
+++ OhmCalculator/OhmCalculator/Controllers/OhmCalculatorController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OhmCalculator.Models;
+using System.Data;
+namespace OhmCalculator.Controllers
+{
+    public class OhmCalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetColorValues()
+        {
+            ViewData["Message"] = "Resistor Calculator";
+            
+            CalculatorModel model = new CalculatorModel()
+            {
+                ColorList = GetColorList(), MultiplierColorList = GetMultiplierColorList(), ToleranceColorList = GetToleranceColorList()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult GetColorValues(CalculatorModel model)
+        {
+            ViewData["Message"] = "Resistor Calculator";
+            model.ColorList = GetColorList();
+            model.MultiplierColorList = GetMultiplierColorList();
+            model.ToleranceColorList = GetToleranceColorList();
+            IOhmValueCalculator calc = new OhmValueCalculator();
+            var result = calc.CalculateOhmValue(model.BandColorA, model.BandColorB, model.Multiplier, model.Tolerance);
+            ViewData["Result"] = result.ToString() +" "+ model.Tolerance.ToString();
+            return View(model);
+        }
+
+        [NonAction]
+        public List<SelectListItem> GetColorList()
+        {
+            string xmlData = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/ColorList.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlData);
+            var Colors = new List<SelectListItem>();
+            Colors = (from rows in ds.Tables[0].AsEnumerable()
+                        select new SelectListItem
+                        {
+                            Text = rows[0].ToString(),
+                            Value =rows[1].ToString(), 
+                        }).ToList();
+            return Colors;  
+        }
+        [NonAction]
+        public List<SelectListItem> GetMultiplierColorList()
+        {
+            string xmlData = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/MultiplierColorList.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlData);
+            var Colors = new List<SelectListItem>();
+            Colors = (from rows in ds.Tables[0].AsEnumerable()
+                      select new SelectListItem
+                      {
+                          Text = rows[0].ToString(),
+                          Value = rows[1].ToString(),
+                      }).ToList();
+            return Colors;
+        }
+        [NonAction]
+        public List<SelectListItem> GetToleranceColorList()
+        {
+            string xmlData = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/ToleranceColorList.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmlData);
+            var Colors = new List<SelectListItem>();
+            Colors = (from rows in ds.Tables[0].AsEnumerable()
+                      select new SelectListItem
+                      {
+                          Text = rows[0].ToString(),
+                          Value = rows[1].ToString(),
+                      }).ToList();
+            return Colors;
+        }
+    }
+  
+}
---
+++ OhmCalculator/OhmCalculator/Views/OhmCalculator/GetColorValues.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<OhmCalculator.Models.CalculatorModel>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	GetColorValues
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+   
+    <h2>
+        <%: ViewData["Message"] %></h2>
+    <form asp-action="GetColorValues" asp-controller="OhmCalculator" method=post>
+        <%= Html.DropDownList("BandColorA", Model.ColorList)%>
+        <%= Html.DropDownList("BandColorB", Model.ColorList)%>
+        <%= Html.DropDownList("Multiplier", Model.MultiplierColorList)%>
+        <%= Html.DropDownList("Tolerance", Model.ToleranceColorList)%>
+        <br /><br />
+        <input type="submit" value="Calculate Ohm" />
+    </form>
+  <br />
+  <h2> Result:    <%: ViewData["Result"]%></h2>
+</asp:Content>
